93446f8656b247989d02c8e5677a29352435aef0
Michael Rodriguez <miarodriguezur@unal.edu.co>
2023-05-07 10:31:40 -0500

    the view header is set

--- UsersApp/Controllers/MainViewController/UsersViewController.swift
+++ UsersApp/Controllers/MainViewController/UsersViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 class UsersViewController: UIViewController {
 
     override func viewDidLoad() {
+        let titleLabel = UILabel()
+        titleLabel.text = "Prueba de ingreso"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        titleLabel.textColor = .white
+        
+        let titleView = UIStackView(arrangedSubviews: [titleLabel])
+        titleView.axis = .vertical
+        titleView.alignment = .leading
+        
+        let leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        leftBarButtonItem.width = -16
+        
+        navigationItem.leftBarButtonItems = [leftBarButtonItem, UIBarButtonItem(customView: titleView)]
+        
+        navigationItem.titleView = titleView
+        
+        navigationController?.navigationBar.backgroundColor = UIColor(red: 0.0/255.0, green: 102.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+      
         super.viewDidLoad()
     }
 
