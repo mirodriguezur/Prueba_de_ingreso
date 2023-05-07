3c821d19e11a3854ca1e15fa72b994c42bd86cbf
Michael Rodriguez <miarodriguezur@unal.edu.co>
2023-05-07 12:20:18 -0500

    The type of result that is obtained when making the user request is defined

---
+++ UsersApp/Model/UsersModel.swift
@@ -0,0 +1,15 @@
+//
+//  UsersModel.swift
+//  UsersApp
+//
+//  Created by Michael Alexander Rodriguez Urbina on 7/05/23.
+//
+
+import Foundation
+
+struct Users: Codable {
+    let id: Int?
+    let name: String?
+    let email: String?
+    let phone: String?
+}
